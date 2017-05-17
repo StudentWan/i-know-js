1f2b21e9c811452588fcd366ee42d534718712d0
StudentWan <benyuwan@gmail.com>
2017-05-17 23:11:58 +0800

    Got data && waiting for analysis

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const router = require('koa-router')()
+const request = require('superagent');
 
-router.get('/', async (ctx, next) => {
+
+router.get('/', async(ctx, next) => {
   await ctx.render('index', {
     title: 'iKnowJs'
+  })
+})
+
+router.post('/search', async(ctx, next) => {
+  const
+    jsLibs = ctx.request.body.values,
+    url = ctx.request.body.url;
+
+  await request.get(url).then((res) => {
+    ctx.body = res.text;
+    //TODO
+  }).catch((err) => {
+    ctx.body = err;
   })
 })
 

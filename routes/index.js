0fb7d7b515c9cca5ba3b12cfe1c479ffd2ebc25f
StudentWan <benyuwan@gmail.com>
2017-05-18 01:16:57 +0800

    change layout && Complete search JS lib function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
 const request = require('superagent');
-
+const fs = require('fs');
 
 router.get('/', async(ctx, next) => {
   await ctx.render('index', {
@@ -14,7 +14,16 @@ router.post('/search', async(ctx, next) => {
     url = ctx.request.body.url;
 
   await request.get(url).then((res) => {
-    ctx.body = res.text;
+    let result = res.text;
+    let data = {};
+    for (let jsLib of jsLibs) {
+      if(result.toLowerCase().indexOf(jsLib.toLowerCase()) >= 0){
+        data[jsLib] = {name: jsLib, isTrue: true};
+      } else {
+        data[jsLib] = {name: jsLib, isTrue: false};
+      }
+    }
+    ctx.body = data;
     //TODO
   }).catch((err) => {
     ctx.body = err;
